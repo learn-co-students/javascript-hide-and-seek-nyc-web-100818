dc9331019cfe0a59ecc13d42a98da859e3c73037
Rexman17 <a.stone1794@gmail.com>
2018-11-20 13:47:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  // pulls a .target out of #nested
+  return document.getElementById('nested').getElementsByClassName('target')[0];
+}
+
+function deepestChild() {
+  const grandNode = document.getElementById('grand-node');
+  const nodes = grandNode.querySelectorAll('div');
+  return nodes[nodes.length - 1];
+}
+
+// Define a function increaseRankBy(n) that increases the ranks in all of the .ranked-lists by n. (You might need to make use of parseInt()
+function increaseRankBy(n) {
+    const ranked = document.getElementsByClassName('ranked-list')
+    for (let i = 0; i < ranked.length; i++) {
+      ranked[i].innerHTML = (parseInt(i) + 1).toString();
+    }
+}
